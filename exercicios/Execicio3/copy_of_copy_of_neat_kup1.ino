1e2fe859171f100b5630df474d628ad2f98b105e
ArturitodaHornet <99339469+ArturitodaHornet@users.noreply.github.com>
2022-03-09 11:49:53 -0300

    Add files via upload

---
+++ exercicios/Execicio3/copy_of_copy_of_neat_kup1.ino
@@ -0,0 +1,44 @@
+// C++ code
+//
+void setup()
+{
+ pinMode(3, OUTPUT);
+ pinMode(6, OUTPUT);
+ pinMode(9, OUTPUT);
+ pinMode(12,OUTPUT);
+ pinMode(10,OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  delay(2000);
+  digitalWrite(6, HIGH);
+  delay(2000); 
+  digitalWrite(9, HIGH);
+  delay(2000); 
+  digitalWrite(12, HIGH);
+  delay(2000); 
+  digitalWrite(10, HIGH);
+  delay(2000); 
+  digitalWrite(3, LOW);
+  delay(2000); 
+  digitalWrite(6, LOW);
+  delay(2000); 
+  digitalWrite(9, LOW);
+  delay(2000); 
+  digitalWrite(12, LOW);
+  delay(2000); 
+  digitalWrite(10, LOW);
+  delay(2000); 
+
+
+
+
+
+
+
+
+
+}
---
+++ exercicios/Execicio4/glorious_duup_bigery1.ino
@@ -0,0 +1,21 @@
+const int botao1 =2;
+const int led1 =3;
+
+int var1;
+
+void setup()
+{
+  pinMode(botao1, INPUT);
+  pinMode(led1, OUTPUT);
+}
+
+void loop()
+{
+  var1 = digitalRead(botao1);
+  if(var1){
+  digitalWrite(led1, HIGH);
+  }else{
+  digitalWrite(led1, LOW);
+  
+}
+}
